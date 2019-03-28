3d4a733e11931aa0c89831e64df3846ae1d52377
Chagil Guiab <chagilguiab@gmail.com>
2019-03-28 16:57:54 -0700

    sets placeholder data in react. stubs api reqs

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,14 +6,34 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      data: true
+      stereos: []
     }
+
+    this.setState = this.setState.bind(this);
+    this.updateQuantity = this.updateQuantity.bind(this);
+  }
+
+  componentDidMount () {
+    $.get('/api/stereos', (results) => {
+      this.setState({stereos: results}, () => {
+        console.log(this.state);
+      });
+    });
+  }
+
+  updateQuantity (quantity) {
+    $.post('/api/stereos', {quantity: quantity}, (results) => {
+      console.log(results);
+    });
   }
 
   render () {
     return (
       <div>
-        Hello World!
+        <h3>Bose SoundTouch 20 Series III Wireless Music System - Black</h3>
+        <p>Frys#: 8574639 Model: 738063-1100</p>
+        <img src="http://images.nymag.com/news/intelligencer/intelposts120326_starstruck_560.jpg" width = "280" height = "115" />
+
       </div>
     )
   }
