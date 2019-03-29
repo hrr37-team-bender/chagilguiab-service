c2d92668c95917538bfe2d5d6e7484ce522c57e9
Chagil Guiab <chagilguiab@gmail.com>
2019-03-28 19:59:02 -0700

    adds getAllStereos function to database/index.js and get endpoint for stereos to server

--- database/index.js
+++ database/index.js
@@ -3,4 +3,16 @@ const mysqlConfig = require('./config.js');
 
 const connection = mysql.createConnection(mysqlConfig);
 
-module.exports = { connection };
+const getAllStereos = function (callback) {
+  let queryString  = `select * from stereos`;
+  connection.query(queryString, (err, results) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    callback(results);
+  });
+};
+
+
+module.exports = { connection, getAllStereos };
--- server/index.js
+++ server/index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const { getAllStereos } = require ('../database/index.js');
 
 app.use(bodyParser());
 app.use(express.static(__dirname + '/../client/dist'));
 
 const port = 3002;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/api/stereos', (req, res) => {
+  getAllStereos((results) => {
+    console.log(results);
+    res.send(results);
+  });
 });
+
+
 
 app.listen(port, () => console.log (`Listening on port ${port}...`));
