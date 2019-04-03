657b09bb79ef4a8ccd0eece0f93de7d9f8385487
Chagil Guiab <chagilguiab@gmail.com>
2019-04-03 13:59:48 -0700

    refactors Cart to be a functional component

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -1,28 +1,12 @@
 import React from 'react';
 import Quantity from './Quantity.jsx';
 
-class Cart extends React.Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      quantity:0,
-      currentPrice: this.props.currentPrice,
-      previousPrice: this.props.previousPrice
-    }
-
-    this.setState = this.setState.bind(this);
-  }
-
-  render () {
-    return (
-      <div>
-        <h4>Price: ${this.props.currentPrice}</h4>
-        <h6>Was: ${this.props.previousPrice}</h6>
-        <Quantity />
-      </div>
-    )
-  }
-}
+const Cart = (props) => (
+<div>
+  <h4>Price: ${props.currentPrice}</h4>
+  <h6>Was: ${props.previousPrice}</h6>
+  <Quantity />
+</div>
+);
 
 export default Cart;
