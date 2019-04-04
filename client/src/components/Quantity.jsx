d5ce09677bbb27ddcd87b776272d77b1ee112c16
Chagil Guiab <chagilguiab@gmail.com>
2019-04-04 15:08:41 -0700

    adds plus and minus buttons to Quantity component

--- client/src/components/Quantity.jsx
+++ client/src/components/Quantity.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 const Quantity = (props) => (
   <div>
+    <button onClick={props.minus}>-</button>
+    0
+    <button onClick={props.plus}>+</button>
     This will be the Quantity console.
   </div>
 );
