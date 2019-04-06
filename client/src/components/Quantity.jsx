035844fc498982ecc70e7f203598afcbcebce579
Chagil Guiab <chagilguiab@gmail.com>
2019-04-05 22:52:16 -0700

    styles quantity component

--- client/src/components/Quantity.jsx
+++ client/src/components/Quantity.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Quantity = (props) => (
   <div className="quantity">
-    <button className="quant-btn" onClick={props.minus}>-</button>
+    <button className="quant-btn" onClick={props.minus}><span className="quant-txt">-</span></button>
     <div className="quant-div">{` ${props.quantity} `}</div>
-    <button className="quant-btn" onClick={props.plus}>+</button>
+    <button className="quant-btn" onClick={props.plus}><span className="quant-txt">+</span></button>
   </div>
 );
 
