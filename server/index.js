8f0f95377764bfcb921c0e942d6c55f9226e116a
Chagil Guiab <chagilguiab@gmail.com>
2019-04-09 17:28:09 -0700

    moves app into a separate Home component. adds React Router and Route

--- server/index.js
+++ server/index.js
@@ -5,18 +5,16 @@ const { getStereo } = require ('../database/index.js');
 
 app.use(bodyParser());
 // app.use(express.static(__dirname + '/../client/dist'));
-const static = `${__dirname}/../client/dist`;
-app.use('/api/stereos/:id', express.static(static));
+const staticPath = `${__dirname}/../client/dist`;
+app.use('/products/:id', express.static(staticPath));
 
 const port = 3002;
 
-app.get('/api/stereos/:id', (req, res) => {
+app.get('/products/:id', (req, res) => {
   let deepfryd_id = req.params.id;
   getStereo(deepfryd_id, (results) => {
     res.send(results);
   });
 });
-
-
 
 app.listen(port, () => console.log (`Listening on port ${port}...`));
