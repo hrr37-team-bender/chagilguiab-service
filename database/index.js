ec2650e8a07bf0219f8e2e84d1623b3ea64463c4
Chagil Guiab <chagilguiab@gmail.com>
2019-04-15 00:11:24 -0700

    refactors backend so client can render any product by id

--- database/index.js
+++ database/index.js
@@ -4,7 +4,7 @@ const mysqlConfig = require('./config.js');
 const connection = mysql.createConnection(mysqlConfig);
 
 const getStereo = function (id, callback) {
-  let queryString  = `select * from stereos where deepfryd_id = '${id}'`;
+  let queryString  = `select * from stereos where id = ${id}`;
   connection.query(queryString, (err, results) => {
     if (err) {
       console.log(err);
